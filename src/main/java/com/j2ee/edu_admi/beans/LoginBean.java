1a860fddf3705c952fe24761fb94d73401455056
dcy <546845583@qq.com>
2022-05-10 23:16:12 +0800

    2022.5.10

--- src/main/java/com/j2ee/edu_admi/beans/LoginBean.java
+++ src/main/java/com/j2ee/edu_admi/beans/LoginBean.java
@@ -2,10 +2,13 @@ package com.j2ee.edu_admi.beans;
 
 import java.sql.*;
 
-public class MysqlConnectTest {
+public class LoginBean {
 
+    //单例模式
+    private static LoginBean loginBean;
+    private Connection connection;
 
-    public boolean Login(String username, String passwd) {
+    private LoginBean() {
         try {
 
             //注册驱动
@@ -13,9 +16,26 @@ public class MysqlConnectTest {
             //连接数据库
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/j2ee", "root", "123456");
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static LoginBean getInstance() {
+        if (loginBean == null) {
+            loginBean = new LoginBean();
+        }
+        return loginBean;
+    }
+
+    public boolean Login(String username, String passwd) {
+        try {
+
             //通过输入用户名向数据库查询
-            Statement statement = conn.createStatement();
-            String sql = "select * from person where username ='"+username+"'and passwd = '"+passwd+"'";
+            Statement statement = connection.createStatement();
+            String sql = "select * from person where username ='" + username + "'and passwd = '" + passwd + "'";
             ResultSet resultSet = statement.executeQuery(sql);
 
             //根据resultSet的结果判断账号密码是否正确
@@ -24,13 +44,10 @@ public class MysqlConnectTest {
             //关闭所有连接
             resultSet.close();
             statement.close();
-            conn.close();
+            connection.close();
 
             return isUserInfoTrue;
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return false;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
--- src/main/java/com/j2ee/edu_admi/beans/UserBean.java
+++
@@ -1,6 +0,0 @@
-package com.j2ee.edu_admi.beans;
-
-public class UserBean {
-    //People表，存放所有人的账号密码
-
-}
