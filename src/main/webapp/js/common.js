c8a81fd0926873002768a58e42610e768cdae574
dcy <546845583@qq.com>
2022-05-18 14:19:53 +0800

    登录注册全部实现5.16---byDcy

---
+++ src/main/webapp/js/common.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+
+    //jquery下拉菜单插件注册
+    $('#menu').tendina({
+        openCallback: function(clickedEl) {
+            clickedEl.addClass('opened');
+        },
+        closeCallback: function(clickedEl) {
+            clickedEl.addClass('closed');
+        }
+    });
+
+});
+$(function(){
+    $("#ad_setting").click(function(){
+        $("#ad_setting_ul").show();
+    });
+    $("#ad_setting_ul").mouseleave(function(){
+        $(this).hide();
+    });
+    $("#ad_setting_ul li").mouseenter(function(){
+        $(this).find("a").attr("class","ad_setting_ul_li_a");
+    });
+    $("#ad_setting_ul li").mouseleave(function(){
+        $(this).find("a").attr("class","");
+    });
+});
